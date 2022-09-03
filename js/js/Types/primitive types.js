ba5ab7b8f66b5006fb89023b2ec48ad7cd4a133b
mohammedyaseen2211 <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-09-03 22:34:14 +0530

    Add files via upload

---
+++ js/js/Types/primitive types.js
@@ -0,0 +1,14 @@
+//  every one say everything is a object 
+// ?but false is not an object .it is value of not object
+// this are the primitive types
+// undefined 
+// String
+// Number
+// Boolean ----- object values not a an object
+// Object
+// null 
+// BigInt
+// these are objects
+objects
+functions
+array
