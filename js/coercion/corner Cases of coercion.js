63a826ae9f234327a6de9d4caa4ca7a62474113b
mohammedyaseen2211 <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-09-04 22:59:11 +0530

    Add files via upload

--- js/coercion/corner Cases of coercion.js
+++ js/coercion/corner Cases of coercion.js
@@ -4,16 +4,26 @@ console.log(Number("\t\n"));
 console.log(Number(null));
 console.log(Number(undefined));
 console.log(Number([]));
-console.log(Number([1,2,3]));
+console.log(Number([1, 2, 3]));
 console.log(Number([null]));
 console.log(Number([undefined]));
 console.log(Number({}));
 
 // strings
-console.log( String("\n" + -0));
-console.log( String(null));
-console.log( String(undefined));
-console.log( String([null]));
-console.log( String([undefined]));
+console.log(String("\n" + -0));
+console.log(String(null));
+console.log(String(undefined));
+console.log(String([null]));
+console.log(String([undefined]));//
+// Number
+console.log(1 < 2);
+console.log(2 < 3);  // this accendiently working
+console.log(1 < 2 < 3);
 
+console.log(3 > 2);
+console.log(2 > 1);
+console.log(3 > 2 > 1);
+console.log(3 > 2) > 1; //so worse code need know
+console.log(true) > 1;
+console.log(1 > 1);
 
