1e7e35d00070509bbe5a4513451a86c5778bcf07
mohammedyaseen2211 <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-09-05 22:53:05 +0530

    Add files via upload

---
+++ js/Equality/double Equals walkthrough.js
@@ -0,0 +1,12 @@
+var number = 10;
+var array = [10];
+
+if (number == array) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+if (number === Number("10")){
+    console.log(typeof number +" "+  typeof [10])
+}
