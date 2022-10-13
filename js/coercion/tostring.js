f8ebb6928428ae7f3745a075946220cc72e7bec8
Mohammad yaseen <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-10-13 22:59:16 +0530

    Add files via upload

--- js/coercion/tostring.js
+++ js/coercion/tostring.js
@@ -2,3 +2,24 @@ var x = 10;
  var b =x.toString();
  console.log(b);
  console.log(typeof b);
+
+ // Tostring  -- function for the conversion of an object to its corresponding value.
+
+ const num  = new Number(34);
+ console.log(num.toString());
+ console.log(typeof num);
+
+
+//  Overriding the default tostring() method
+
+
+function myobj(name,age){
+    this.name = name;
+    this.age = age;
+}
+const obj = new myobj('roshan',21);
+
+myobj.prototype.toString =function fun() {
+    return this.name + this.age 
+}
+console.log(obj.toString())
