2693e041ef6a91daa618a606e803b9efc20be723
Mohammad yaseen <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-10-18 23:38:58 +0530

    Add files via upload

---
+++ js/Object/Object.js
@@ -0,0 +1,21 @@
+// javascript objects are containers for named values clled properties. Object mehtod object can also have methods. Methods are action that can be performed.
+
+// Example
+
+var object = {
+   name:"yaseen",
+   age:21,
+
+}
+console.log(object)
+
+var x  = "yaseen"
+
+function outerfunc(){
+     console.log(x); // doubt
+    // var x = "aaqib"
+    // console.log(x);
+
+}
+outerfunc();
+console.log(x);
