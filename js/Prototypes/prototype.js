d8c61131ecb0de791df25fd0a3b342a095deeeec
Mohammad yaseen <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-10-20 00:02:13 +0530

    Add files via upload

--- js/Prototypes/prototype.js
+++ js/Prototypes/prototype.js
@@ -1,3 +1,5 @@
 //  A constructor call makes an object based on its own prototype
 
 //  A constructor call makes an object linked to its own prototype.
+
+// Protypes are the mechanism by which javascript objects inheritance from one another. in this article we explain what a prototype is how
