e2349af26e8d77cc498d673477664fe31c6fb3c6
Mohammad yaseen <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-10-21 23:57:37 +0530

    Add files via upload

--- js/Object/Refernce.js
+++ js/Object/Refernce.js
@@ -1,5 +1,5 @@
-// Object refernce
-//  A variable assigned to an object stores not the object itself but its addressing memory
+// Object reference
+//  A variable assigned to an object doesn't stores the object itself but its addressing memory
 
 var obj1 = {
     a:20,b:30,
@@ -11,7 +11,7 @@ console.log(obj1);
 
 // HERE above the object values are changing it not copying the value
 
-// non refernce
+// non reference
 
 var x = 10;
 var temp = x;
@@ -20,3 +20,23 @@ console.log("temp value :" + temp)
 var x = 20;
 console.log("After Assign:" +  x);
 console.log("After assign temp value :" + temp); // no changed because it is assigned before 20
+
+// another example
+
+var a = 10 ;
+var b = 20;
+var c = a ;
+console.log(c);
+var a = 20;
+console.log(c);
+
+//  with objects
+
+var a = {
+    value :25,
+}
+var b = a ;
+console.log(b.value);
+a.value = 35;
+console.log(b.value);
+// console.log(a)
