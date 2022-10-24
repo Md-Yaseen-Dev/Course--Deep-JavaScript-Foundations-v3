1a42b78227f9575a111cd8ba9a4143d19558d3e3
Mohammad yaseen <90973035+mohammedyaseen2211@users.noreply.github.com>
2022-10-24 17:36:23 +0530

    Add files via upload

--- js/Types/primitive types.js
+++ js/Types/primitive types.js
@@ -9,6 +9,89 @@
 // null 
 // BigInt
 // these are objects
-objects
-functions
-array
+// objects
+// functions
+// array
+
+
+
+// for example :- Number
+
+var a = 15 ;
+
+console.log(typeof a );
+
+// for example:- string
+
+var b = "15";
+
+console.log(typeof b);
+
+// for example :- boolean 
+
+var c = true;
+console.log(typeof c);
+
+// for example:- null
+
+var d = null ;  // null is the absence of value
+console.log(typeof d);
+
+// for example :- undefined
+
+var e =  undefined; // value is not defined
+var f ;         // it is an example of undefined
+console.log(typeof e);
+console.log(typeof f);
+
+// for example:- big integer
+
+var g = 12n;
+console.log(typeof g);
+
+
+// non primtives 
+
+//  for example:- array
+
+var array = [1,2,3];
+console.log(typeof array);
+
+var array1 = new Array(4,5,6); // we can create array in this type to by using new keyword
+console.log(array1 +" " + typeof array1);
+
+//for example :-  object
+
+var object = {
+    name: "kyle simpson",
+    course: "deep javascript"
+}
+console.log(object);
+console.log(typeof object )
+
+var object1 = new Object();
+object1.name = "yaseen"
+object1.age = "21";
+
+console.log(object1);
+console.log(typeof object1);
+
+//  also we can create object using function with this keyword
+
+function object3(name,age){
+this.name = name;
+this.age = age;
+}
+s = new object3("sdf", 34);
+console.log(typeof s)
+
+
+// for example :- function 
+
+function myfunc(){
+    var name  = "oden";
+    console.log(name);
+
+}
+myfunc();
+console.log(typeof myfunc)
