0973626c2dba36a666d61b534a54a0a34d50e7e5
Mohammad Yaseen <90973035+Md-Yaseen-Dev@users.noreply.github.com>
2022-12-09 01:14:31 +0530

    Add files via upload

--- js/coercion/cases of coercion.js
+++ js/coercion/cases of coercion.js
@@ -11,8 +11,8 @@ console.log(`there are ${String(numstudents)} players.`);
 function add_student(numstudents) {
     return numstudents + 1;
 }
-var studentsInputElem = 16;
-var b = add_student(+studentsInputElem);
+var studentsInputElem = "16";
+var b = add_student(studentsInputElem);
 console.log(b);
 console.log(add_student(89));
 // - operator
@@ -21,7 +21,21 @@ function kick_student(numstudents) {
 }
 console.log(kick_student(25));
 
-var newstudents = [3,4];
+var newstudents = [3, 4];
 while (newstudents.length > 0) {
- console.log(kick_student(newstudents.pop()))
+    console.log(kick_student(newstudents.pop()))
 }
+
+///---new
+
+function addstudents(numstudent) {
+    return numstudent + 1
+}
+
+console.log(addstudents("16")); // "161"
+console.log(addstudents(+"16")); // 17
+
+
+
+// the unary plus operator if you look in the spec for it that the two number abstract operation occurs
+
